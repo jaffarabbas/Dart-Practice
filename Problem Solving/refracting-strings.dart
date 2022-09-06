3cba09ed4b9473743d2ce8a65a554379db0ec709
Jaffar Abbas <gamaportal8@gmail.com>
2022-09-06 19:44:36 +0500

    Inserted refracting-strings.dart at 06/09/2022 19:44:35

---
+++ Problem Solving/refracting-strings.dart
@@ -0,0 +1,4 @@
+void main(){
+  String s = 'DD HackerRankProblemSolving/HackerRankProblemSolving/2D Array - DS.cs';
+  print(s.replaceAll('"'," ").replaceAll("??","").replaceAll("MM","").replaceAll("DD","").trim());
+}
